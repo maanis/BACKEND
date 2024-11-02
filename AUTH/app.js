8612de4dfd40b6f69f2f5a76e8dfe1d1f09ebe2b
Manish Jha <manishjha962@gmail.com>
2024-11-02 19:20:01 +0530

    bcrypt tak ka safar

--- AUTH/app.js
+++ AUTH/app.js
@@ -8,6 +8,7 @@ var flash = require('connect-flash')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 app.use(session({
--- AUTH/routes/index.js
+++ AUTH/routes/index.js
@@ -1,64 +1,60 @@
 var express = require('express');
 var router = express.Router();
-const session = require('express-session')
+const bcrypt = require('bcryptjs')
+const userModel = require('./users')
 
 router.get('/', async function (req, res, next) {
   res.render('index')
 });
 
-//cookies
-// router.get('/set', async function (req, res, next) {
-//   res.cookie('name', 'manish')
-//   res.send('created')
-// });
+router.post('/register', async function (req, res, next) {
+  const { name, email, password } = req.body
 
-// router.get('/read', async function (req, res, next) {
-//   console.log(req.cookies.name)
-//   res.send('check kar bhava')
-// });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      const dets = await userModel.create({
+        name,
+        email,
+        password: hash
+      })
+      res.render('user', { name, email })
+    })
+  })
 
-// router.get('/delete', async function (req, res, next) {
-//   res.clearCookie('name')
-//   console.log(req.cookies)
-//   res.send('deleted')
-// });
-
-
-//session
-// router.get('/set', (req, res) => {
-//   req.session.age = 12;
-//   req.session.username = 'manish';
-//   // Setting a session variable
-//   res.send('Session variable set');
-//   console.log(req.session)
-// });
-
-// router.get('/read', function (req, res, next) {
-//   console.log(req.session.username)
-//   res.send('check kar bhava')
-// });
-
-// router.get('/delete', function (req, res, next) {
-//   req.session.destroy()
-//   console.log(req.session)
-//   res.send('check kar bhava')
-// });
-
-
-//flash
-router.get('/set', (req, res) => {
-  req.flash('name', 'manish')
-  req.flash('name', { name: 'manish', age: 21, ban: false })
-  res.send('bann gaya')
-});
-
-router.get('/get', (req, res) => {
-  console.log(req.flash('name'));
-  res.send('bann gaya')
 });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// // to hash a password
+// router.get('/password', async function (req, res, next) {
+//   bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash('password', salt, (err, hash) => {
+//       console.log(hash)
+//       req.session.pass = hash
+//       res.send('bann gaya')
+//     })
+//   })
+// });
+
+// //to decrypt
+// router.get('/decrypt', async function (req, res, next) {
+//   bcrypt.compare('password', req.session.pass, (err, result) => {
+//     console.log(result)
+//   })
+
+//   res.send('check')
+// });
 
 
 
