0b8cb89cba2453f5c95b3b0ef84d764c4692036a
Manish Jha <manishjha962@gmail.com>
2024-11-03 16:07:23 +0530

    done authentication

--- AUTH/routes/index.js
+++ AUTH/routes/index.js
@@ -10,6 +10,10 @@ router.get('/', async function (req, res, next) {
 
 router.post('/register', async function (req, res, next) {
   const { name, email, password } = req.body
+  let user = await userModel.findOne({ email })
+  if (user) {
+    return res.send('user already exist')
+  }
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(password, salt, async (err, hash) => {
       const dets = await userModel.create({
@@ -17,9 +21,9 @@ router.post('/register', async function (req, res, next) {
         email,
         password: hash
       })
-      let token = jwt.sign({ email }, 'shhh')
+      let token = jwt.sign({ email, name }, 'shhh')
       res.cookie('token', token)
-      res.render('user', { name, email })
+      res.redirect('profile')
     })
   })
 });
@@ -28,24 +32,46 @@ router.get('/login', (req, res) => {
   res.render('login')
 });
 
-router.post('/login', async function (req, res, next) {
 
+
+router.post('/login', async function (req, res, next) {
   const { email, password } = req.body
   let user = await userModel.findOne({ email })
   if (!user) {
     return res.status(400).send('User not found.')
   }
-
-  
-
+  bcrypt.compare(password, user.password, (err, result) => {
+    console.log(result)
+    if (result) {
+      let token = jwt.sign({ email, name: user.name }, 'shhh')
+      res.cookie('token', token)
+      res.redirect('profile')
+    }
+    else {
+      res.send('somethimg went wrong')
+    }
+  })
 })
 
-
+router.get('/profile', isLoggedIn, (req, res) => {
+  console.log(req.data)
+  res.render('profile', { name: req.data.name, email: req.data.email })
+});
 router.get('/logout', (req, res) => {
-  res.clearCookie('token')
+  res.cookie("token", "")
   res.redirect('/')
 });
 
+
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "") {
+    res.send('you must be log in')
+  } else {
+    let data = jwt.verify(req.cookies.token, "shhh")
+    req.data = data
+    next()
+  }
+}
 
 
 
