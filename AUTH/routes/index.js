903e20ba01f25951fdbb21d126a45e202bcb52c0
Manish Jha <manishjha962@gmail.com>
2024-11-03 11:57:22 +0530

    tabnine

--- AUTH/routes/index.js
+++ AUTH/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcryptjs')
 const userModel = require('./users')
+const jwt = require('jsonwebtoken')
 
 router.get('/', async function (req, res, next) {
   res.render('index')
@@ -9,7 +10,6 @@ router.get('/', async function (req, res, next) {
 
 router.post('/register', async function (req, res, next) {
   const { name, email, password } = req.body
-
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(password, salt, async (err, hash) => {
       const dets = await userModel.create({
@@ -17,10 +17,33 @@ router.post('/register', async function (req, res, next) {
         email,
         password: hash
       })
+      let token = jwt.sign({ email }, 'shhh')
+      res.cookie('token', token)
       res.render('user', { name, email })
     })
   })
+});
 
+router.get('/login', (req, res) => {
+  res.render('login')
+});
+
+router.post('/login', async function (req, res, next) {
+
+  const { email, password } = req.body
+  let user = await userModel.findOne({ email })
+  if (!user) {
+    return res.status(400).send('User not found.')
+  }
+
+  
+
+})
+
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('token')
+  res.redirect('/')
 });
 
 
